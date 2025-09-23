645e8026b26b5909a96ffe9800e3a944d462a092
Paulo Andres Berrios Diaz <extpberrios@transbank.cl>
2025-09-23 15:22:11 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import react from 'react';
+import Navbar from './components/Navbar';
+import Mensajeprincipal from './components/Mensaje-principal';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar/>
+      <Container>
+        <Mensajeprincipal/>
+        <Mensajeprincipal/>
+        <Mensajeprincipal/>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Mensaje-principal.js
@@ -0,0 +1,17 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import logo from '../logo.svg';
+
+function Mensajeprincipal() {
+  return (
+    <Row>
+        <Col md={6}>
+        <p>lorem dosaud poasidu asodiasudoiasudsoaiudoasiu</p>
+        </Col>
+        <Col md={6}>
+        <img src={logo}></img>
+        </Col>
+    </Row>
+  );
+}
+
+export default Mensajeprincipal;
