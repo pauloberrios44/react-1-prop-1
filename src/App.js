3c1d8f009a7180c5c8cb8edb20b683ca0fb93ebe
Paulo Andres Berrios Diaz <extpberrios@transbank.cl>
2025-09-29 19:13:07 -0300

    avances programacion

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import './App.css';
 import react from 'react';
 import Navbar from './components/Navbar';
 import Mensajeprincipal from './components/Mensaje-principal';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className='App'>
       <Navbar/>
       <Container>
-        <Mensajeprincipal/>
-        <Mensajeprincipal/>
         <Mensajeprincipal/>
       </Container>
     </div>
